f281dd324fd767c7cfa23297e374aa02a539cec7
chexq <chexq35@gmail.com>
2024-01-09 23:11:04 -0500

    use sass

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { Container } from "react-bootstrap";
 
 function Home(props) {
-    return <div>This is my home page</div>;
+    return (
+        <Container>
+            <div>This is my home page</div>
+        </Container>
+    );
 }
 
 export default Home;
